5f5e48352067dd5a4cec01251886666a73d9067d
daniellelsilva <danielle.luisasilva@gmail.com>
2024-02-19 10:59:03 -0300

    create grid item

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Pressable, Text, View } from 'react-native'
+
+export default function CategoryGridTile({title, color}) {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  )
+}
