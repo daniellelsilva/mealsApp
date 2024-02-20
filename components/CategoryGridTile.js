7712db6f8a83a64e93dbb05f20e5360401969600
daniellelsilva <danielle.luisasilva@gmail.com>
2024-02-20 11:06:10 -0300

    WIP

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,14 +1,54 @@
 import React from 'react'
-import { Pressable, Text, View } from 'react-native'
+import { Platform, Pressable, StyleSheet, Text, View } from 'react-native'
 
-export default function CategoryGridTile({title, color}) {
+export default function CategoryGridTile({title, color, onPress}) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{color: "#ccc"}}
+        style={({pressed}) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null
+        ]}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, {backgroundColor: color}]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible"
+  },
+  button: {
+    flex: 1
+  },
+  buttonPressed: {
+    opacity: 0.5
+  },
+  innerContainer: {
+    borderRadius: 8,
+    flex: 1,
+    padding: 16,
+    justifyContent:"center",
+    alignItems: "center"
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  }
+});
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { FlatList, ScrollView } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
-export default function CategoriesScreen() {
+export default function CategoriesScreen({ navigation }) {
+
+  
   const renderCategoryItem = (itemData) => {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+    const onPressHandler = () => {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      })
+    }
+
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={onPressHandler} />
   }
 
   return (
-    <ScrollView>
+    <View>
       <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item.id}
         renderItem={renderCategoryItem}
+        numColumns={2}
       />
-    </ScrollView>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  
+});
