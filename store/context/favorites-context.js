515a3d138e6739ed9f6f29a0b7c27ff4538d07ce
daniellelsilva <danielle.luisasilva@gmail.com>
2024-02-27 11:30:47 -0300

    improve context

--- store/context/favorites-context.js
+++ store/context/favorites-context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const FavoritesContext = createContext({
@@ -8,5 +8,20 @@ export const FavoritesContext = createContext({
 })
 
 export function FavoritesContextProvider({children}) {
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  const addFavorite = (id) => {
+    setFavoriteMealIds((prevState) => [...prevState, id])
+  }
+
+  const removeFavorite = (id) => {
+    setFavoriteMealIds((prevState) => prevState.filter((mealId) => mealId !== id))
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite
+  }
+  return <FavoritesContext.Provider value={value} >{children}</FavoritesContext.Provider>
 }
