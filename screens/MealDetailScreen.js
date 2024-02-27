345c220415b975e448c2604dd197ee9ee548ad4e
daniellelsilva <danielle.luisasilva@gmail.com>
2024-02-27 11:15:16 -0300

    improve meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import MealDetails from '../components/MealDetails'
 import { MEALS } from '../data/dummy-data';
+import IconButton from '../components/IconButton';
 
-export default function MealDetailScreen({ route }) {
+export default function MealDetailScreen({ route, navigation }) {
   const { mealId } = route.params;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  // const onPressHandler = () => console.log('PRESSED')
+  
+
+  // useLayoutEffect(() => {
+  //   navigation.setOptions({
+  //     headerRight: () => {
+  //       return <IconButton icon={"star"} color={"white"} onPress={onPressHandler} />
+  //     }
+  //   })
+  // })
 
   return (
     <ScrollView style={styles.rootContainer}>
